3527419e55041d09c07a62af7ce8db116b8d9c7d
Manuel Garcia <garciahildo@gmail.com>
2023-05-03 21:53:45 -0500

    Primary phase of calculator with possible use

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 'use strict'
 
-/** Initialize var result */
+/** Initialize var save result, prevNum and status*/
+var prevNum;
 var result;
 localStorage.setItem('optionCal', 0)
 
@@ -10,9 +11,6 @@ localStorage.setItem('optionCal', 0)
  * @param {option} option Option to realise
  */
 function setValue(value, option) {
-    console.log(value);
-    console.log(option);
-
     switch (option) {
         case 1:
             /** Save operator */
@@ -22,30 +20,56 @@ function setValue(value, option) {
             elemNumber.innerHTML += value;
 
             break;
+
         case 2:
+            let elemCalc = document.getElementById('text_calc');
+
             /** Validate first option is operator */
             if (localStorage.getItem('optionCal') == '0') {
-                value = "0" + value; 
+                prevNum = 0;
+                value = "0" + value;
             }
 
             /** Save operator */
             localStorage.setItem('optionCal', 2);
 
             if (value != '=') {
-                let elemCalc = document.getElementById('text_calc');
-                elemCalc.innerHTML += value;    
-                return;
+                prevNum = Number(elemCalc.innerHTML);
+                elemCalc.innerHTML += value;
             }
-
-            let getData = document.getElementById('text_calc');
-            let saveResult = document.getElementById('result');
-            saveResult.innerHTML = getData;
 
             break;
         default:
             alert('Ocurrió algo inesperado, por favor recargue e intente nuevamente.')
             break;
     }
+
+}
+
+/** 
+ * Get result
+ */
+function getResult(){
+
+    var elemCalc = document.getElementById('text_calc');
+
+    if (prevNum === null) {
+        let currNum = Number(elemCalc.innerHTML);
+        let saveResult = document.getElementById('result');
+        saveResult.innerHTML = currNum;
+        prevNum = null;
+        localStorage.setItem('optionCal', 0);
+        elemCalc.innerHTML = "";
+        return
+    }
+
+    let currExpr = elemCalc.innerHTML;
+    let currNum = eval(currExpr);
+    let saveResult = document.getElementById('result');
+    saveResult.innerHTML = currNum;
+    prevNum = null;
+    localStorage.setItem('optionCal', 0);
+    elemCalc.innerHTML = "";
 
 }
 
@@ -56,8 +80,8 @@ function cleanAll() {
     let docTextCalc = document.getElementById('text_calc');
     docTextCalc.innerHTML = "";
 
-    /*let saveResult = document.getElementById('result');
-    saveResult.innerHTML = 0;*/
+    let saveResult = document.getElementById('result');
+    saveResult.innerHTML = "";
 
     localStorage.setItem('optionCal', 0)
 }
